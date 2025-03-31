f0929358c9f5269c1a2252300a688f1233b4c89e
bhalawaty <bilal.elhalawaty@foo.mobi>
2025-03-31 03:53:11 +0200

    add DB connection with request struct

--- Starter/Request.go
+++ Starter/Request.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 type Request struct {
-	Context *gin.Context
+	Context    *gin.Context
+	DB         *gorm.DB
+	Connection *sql.DB
 }
 
 func req() func(c *gin.Context) Request {
 	return func(c *gin.Context) Request {
 		var request Request
 		request.Context = c
+		request.DB, request.Connection = db()
+
 		return request
 	}
 }
@@ -20,4 +28,19 @@ func getRequest(c *gin.Context) Request {
 	req := req()
 	request := req(c)
 	return request
+}
+
+func db() (*gorm.DB, *sql.DB) {
+	dsn := "root:123456@tcp(127.0.0.1:3306)/GoLang?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("there is a connect issue :", err.Error())
+	}
+
+	connection, err := db.DB()
+
+	if err != nil {
+		fmt.Println("there is a connection issue :", err.Error())
+	}
+	return db, connection
 }
--- Starter/main.go
+++ Starter/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,6 +12,9 @@ func main() {
 	application := app().Gin //run the application
 	application.GET("/ping", func(c *gin.Context) {
 		request := getRequest(c)
+		fmt.Println("ping before close ", request.Connection.Ping())
+		request.Connection.Close()
+		fmt.Println("ping after close ", request.Connection.Ping())
 		request.Context.JSON(http.StatusOK, gin.H{
 			"message": "pooooong",
 		})
